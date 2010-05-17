52a36a8d2afcfed07fa487eeca160f8e0d2de2d3
Henry Tan <ztan@gwu.edu>
2010-05-17 19:34:13 -0400

    Completed exercises in Chapter3

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,7 +7,11 @@ describe PagesController do
     controller.should be_an_instance_of(PagesController)
   end
 
-
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -17,7 +21,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | Home")
+                               @base_title + " | Home")
     end
     
   end
@@ -31,7 +35,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | Contact")
+                               @base_title + " | Contact")
     end
     
   end
@@ -45,9 +49,23 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | About")
+                               @base_title + " | About")
     end
     
-  end  
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_tag("title",
+                               @base_title + " | Help")
+    end
+    
+  end
 
 end
